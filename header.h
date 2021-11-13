486b51b26623d66fd100e6f835ea715f276929ab
phuvinhnguyen <81418873+phuvinhnguyen@users.noreply.github.com>
2021-11-13 12:57:03 +0700

    Add files via upload

--- header.h
+++ header.h
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <Windows.h>
+#ifndef HEADER
+#define HEADER
+
+#include "const.h"
 using namespace std;
 
-void gotoxy(int x, int y)
-{
-    static HANDLE h = NULL;
-    if (!h)
-        h = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD c = {x, y};
-    SetConsoleCursorPosition(h, c);
-}
+// FUNCTION ALL CONSOLE FUNCTION
+
+void gotoxy(int x, int y);
+void FixConsoleWindow();
+void SetWindowSize(SHORT width, SHORT height);
 
 // DEFINE ALL THE MAIN CLASS
 
@@ -20,30 +16,31 @@ struct OXY
 {
     int x, y;
     OXY(int x, int y) : x(x), y(y) {}
+
+    bool operator==(OXY &b)
+    {
+        return (b.x == x) && (b.y == y);
+    }
 };
 
 // cây để lưu vị trí các điểm đã bị vật chiếm
 // các điểm có trong cây nếu player chạm phải = die
-class Tree // vinh
-{
-    void insert(OXY);
-    void remove(OXY);
-    bool exist(OXY);
-};
+class Tree;
 
 class animalLane // hoàng
 {
 private:
     /* data */
 public:
-    animalLane(Tree &t, int line, int level);
+    animalLane(Tree &t, int line, int level, int lineNum = 5);
     ~animalLane();
 
     // tạo ra 1 lane có động vật chạy qua lại
     // lane ở dòng thứ line, độ khó là level, lưu data vào t
+    // lane có chiều rộng là lineNum(để mặc định là 5)
     // hàm được gọi lại nhiều lần, mỗi lần sẽ cập nhật vị trí khác nhau
     // nên có tầm 3,4 loại động vật-
-    void run(OXY playerPos, int line);
+    void run();
 };
 
 class carLane // nhân
@@ -51,32 +48,19 @@ class carLane // nhân
 private:
     /* data */
 public:
-    carLane(Tree &t, int line, int level);
+    carLane(Tree &t, int line, int level, int lineNum = 5);
     ~carLane();
 
     // tạo ra 1 lane có xe chạy qua lại
     // lane ở dòng thứ line, độ khó là level, lưu data vào t
+    // lane có chiều rộng là lineNum(để mặc định là 5)
     // hàm được gọi lại nhiều lần, mỗi lần sẽ cập nhật vị trí khác nhau
     // nên có tầm 3,4 kiểu xe-
     // nhớ có đèn giao thông
-    void run(OXY playerPos, int line);
+    void run();
 };
 
-class player // vinh
-{
-private:
-    /* data */
-public:
-    player();
-    ~player();
-
-    // hàm điều khiển người chơi
-    // cập nhật vị trí ng chơi
-    OXY run();
-
-    // cập nhật vị trí người chơi
-    OXY position();
-};
+class player;
 
 // class này tạo menu
 // menu gồm điểm của ng chơi
@@ -110,3 +94,41 @@ public:
     // tự chọn âm thanh, hiệu ứng
     int loadGame(int level, string filePath = "", bool music = 0);
 };
+
+// CONSOLE FUNCTION CODE
+
+void gotoxy(int x, int y)
+{
+    static HANDLE h = nullptr;
+    if (!h)
+        h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD c = {x, y};
+    SetConsoleCursorPosition(h, c);
+}
+
+void FixConsoleWindow()
+{
+    HWND consoleWindow = GetConsoleWindow();
+    LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+    style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+    SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+void SetWindowSize(SHORT width, SHORT height)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD NewSize;
+    SMALL_RECT WindowSize;
+
+    WindowSize.Top = 0;
+    WindowSize.Left = 0;
+    WindowSize.Bottom = height - 1;
+    WindowSize.Right = width - 1;
+    NewSize.X = width;
+    NewSize.Y = height;
+
+    SetConsoleWindowInfo(hStdout, 1, &WindowSize);
+    SetConsoleScreenBufferSize(hStdout, NewSize);
+}
+
+#endif
